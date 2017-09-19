0cf60d7f4dccf0629b9131756bcadaee042a5487
Yongjin Park <ypp@csail.mit.edu>
2017-09-18 20:29:55 -0400

    change of null distribution

--- make.mediation.R
+++ make.mediation.R
@@ -2,24 +2,24 @@
 
 argv <- commandArgs(trailingOnly = TRUE)
 
-ld.file <- argv[1]                      # e.g., 'stat/IGAP/ld/hs-fqtl/19.obs_ld.gz'
-sum.file <- argv[2]                     # e.g., 'stat/IGAP/hs-fqtl/19.obs_bed.gz'
-plink.hdr <- argv[3]                    # e.g., '1kg/chr19' # geno/rosmap1709-chr19'
-ld.idx <- as.integer(argv[4])           # e.g., 117
-gwas.sample.size <- as.numeric(argv[5]) # e.g., 74000
-qtl.sample.size <- as.numeric(argv[6])  # e.g., 300
+ld.file <- argv[1]                      # e.g., ld.file = 'stat/IGAP/ld/hs-fqtl/19.obs_ld.gz'
+sum.file <- argv[2]                     # e.g., sum.file = 'stat/IGAP/hs-fqtl/19.obs_bed.gz'
+plink.hdr <- argv[3]                    # e.g., plink.hdr = '1kg/chr19' # geno/rosmap1709-chr19'
+ld.idx <- as.integer(argv[4])           # e.g., ld.idx = 117
+gwas.sample.size <- as.numeric(argv[5]) # e.g., gwas.sample.size = 74000
+qtl.sample.size <- as.numeric(argv[6])  # e.g., qtl.sample.size = 300
 out.hdr <- argv[7]                      # e.g., 'temp'
 
 dir.create(dirname(out.hdr), recursive = TRUE)
 
-zqtl.num.boot <- 101
+zqtl.num.boot <- 111
 
 source('util.R')
 options(stringsAsFactors = FALSE)
 
-z.out.file <- out.hdr %&&% '.zqtl.txt.gz'
-egger.out.file <- out.hdr %&&% '.egger.txt.gz'
-rot.egger.out.file <- out.hdr %&&% '.rot.egger.txt.gz'
+z.out.file <- out.hdr %&&% '.zqtl.gz'
+egger.out.file <- out.hdr %&&% '.egger.gz'
+rot.egger.out.file <- out.hdr %&&% '.rot-egger.txt.gz'
 
 if(file.exists(z.out.file)) {
     log.msg('File exists : %s\n\n\n', z.out.file)
@@ -33,13 +33,12 @@ library(broom)
 source('mediation.R')
 
 ################################################################
-ld.tab <- read.table(ld.file, col.names = c('chr', 'ld.lb', 'ld.ub', 'n.snp.ld', 'n.qtl.ld'))
-ld.info <- ld.tab[ld.idx, ]
-
-################################################################
 ## temporary directory
 temp.dir <- system('mktemp -d ' %&&% out.hdr %&&% '-temp.XXXX',
                    intern = TRUE, ignore.stderr = TRUE)
+
+ld.tab <- read.table(ld.file, col.names = c('chr', 'ld.lb', 'ld.ub', 'n.snp.ld', 'n.qtl.ld'))
+ld.info <- ld.tab[ld.idx, ]
 
 plink <- subset.plink(ld.info, temp.dir)
 x.bim <- data.frame(plink$BIM, x.pos = 1:nrow(plink$BIM))
@@ -68,10 +67,10 @@ log.msg('Finished Rotated EGGER\n\n\n')
 
 ################################################################
 ## 3. zqtl
-vb.opt <- list(pi.lb = -4, pi.ub = -1, tol = 1e-8, gammax = 1e4,
+vb.opt <- list(pi = -2, tau = -4, do.hyper = FALSE, tol = 1e-8, gammax = 1e3,
                vbiter = 7500, do.stdize = TRUE, eigen.tol = 1e-2,
                rate = 1e-2, decay = -1e-2,
-               print.interv = 500, nboot = zqtl.num.boot)
+               print.interv = 1500, nboot = zqtl.num.boot)
 
 z.out <- fit.med.zqtl(zqtl.data$gwas.theta, zqtl.data$gwas.se,
                       zqtl.data$qtl.theta, zqtl.data$qtl.se,
@@ -79,20 +78,19 @@ z.out <- fit.med.zqtl(zqtl.data$gwas.theta, zqtl.data$gwas.se,
                       n.med = qtl.sample.size, options = vb.opt)
 
 boot.tab <- melt.effect(list(lodds.boot.mean = z.out$boot.mediated.lodds.mean,
-                             lodds.boot.se = sqrt(z.out$boot.mediated.lodds.var),
-                             theta.boot.mean = z.out$boot.mediated.theta.mean,
-                             theta.boot.se = sqrt(z.out$boot.mediated.theta.var)),
+                             lodds.boot.se = sqrt(z.out$boot.mediated.lodds.var)),
                         genes$ensg, 'boot') %>%
                             rename(ensg = Var1) %>% dplyr::select(-Var2)
 
 out.tab <- melt.effect(z.out$param.mediated, genes$ensg, 'obs') %>%
     rename(ensg = Var1) %>% dplyr::select(-Var2) %>%
         left_join(genes, by = 'ensg') %>%
-            left_join(boot.tab, by = 'ensg')
+            left_join(boot.tab, by = 'ensg') %>%
+                left_join(data.frame(ensg = genes$ensg, emp.pval = z.out$mediation.pvalue),
+                          by = 'ensg')
 
 out.tab <- out.tab %>%
-    mutate(lodds.l10p = l10.p.one((lodds - lodds.boot.mean)/(1e-2 + lodds.boot.se))) %>%
-        mutate(theta.l10p = l10.p.two(abs(theta / (1e-2 + sqrt(theta.var)) - theta.boot.mean)/(1e-2 + theta.boot.se)))
+    mutate(z.pval = pnorm((lodds - lodds.boot.mean) / (1e-2 + lodds.boot.se), lower.tail = FALSE))
 
 write.tab(out.tab, file = gzfile(z.out.file))
 write.tab(egger.tab, file = gzfile(egger.out.file))
