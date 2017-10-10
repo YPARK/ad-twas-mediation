53155d4d486ba22b640d938d7549aa1c55b46151
Yongjin Park <ypp@csail.mit.edu>
2017-10-10 12:23:18 -0400

    more global figures

--- figure.bootstrap.R
+++ figure.bootstrap.R
@@ -13,8 +13,126 @@ source('figure.util.R')
 
 gene.tab.file <- 'tables/bootstrap_gene.txt.gz'
 gene.tab <- read.table(gene.tab.file, header = TRUE) %>% na.omit()
+
 gene.tab.sig.file <- 'tables/bootstrap_gene_significant.txt.gz'
 gene.tab.sig <- read.table(gene.tab.sig.file, header = TRUE) %>% na.omit()
+
+gene.tab.strict.file <- 'tables/bootstrap_gene_strict.txt.gz'
+gene.tab.strict <- read.table(gene.tab.strict.file, header = TRUE) %>% na.omit()
+
+gene.tab <- gene.tab %>% group_by(hgnc) %>%
+    slice(which.max(lodds))
+
+gene.tab.sig <- gene.tab.sig %>% group_by(hgnc) %>%
+    slice(which.max(lodds))
+
+gene.tab.strict <- gene.tab.strict %>% group_by(hgnc) %>%
+    slice(which.max(lodds))
+
+## Figure 0. just combine all
+.genome.mb <- function() {
+    function(x) format(x/1e6, big.mark=',')
+}
+
+get.genome.scale <- function(df, blk.width = 1e6) {
+
+    genome.range <- range(c(df$tss, df$tes))
+    genome.length <- genome.range[2] - genome.range[1]
+    genome.w <- genome.length / blk.width * 0.05 + 1
+
+    genome.x.scale <- scale_x_continuous(limits = genome.range + c(-1000, 1000), expand = c(0, 0),
+                                         labels = .genome.mb())
+
+    return(genome.x.scale)
+}
+
+x.scale <- get.genome.scale(gene.tab, blk.width = 1e6)
+
+.thm <- theme(plot.background = element_blank(),
+              panel.background = element_blank(),
+              strip.background = element_blank())
+              
+plt <- ggplot(gene.tab) + theme_classic() + .thm +
+    theme(axis.text.x = element_text(size = 6, angle = 30)) +
+    xlab('genomic location (mb)') +
+    ylab('mediation effect') +
+    scale_x_continuous(labels = .genome.mb())
+
+.aes.lr <- aes(x = (tss + tes)/2,
+               ymin = theta - 2 * theta.se,
+               ymax = theta + 2 * theta.se)
+
+plt <- plt + geom_linerange(data = gene.tab.strict, .aes.lr, alpha = 0.5)
+
+.aes <- aes(x = (tss + tes)/2, y = theta, color = (chr %% 2 == 0),
+            alpha = 1/(1+exp(-lodds)), size = 1/(1+exp(-lodds)))
+
+plt <- plt + geom_point(.aes, show.legend = FALSE) +
+    scale_color_manual(values = c('#3333FF', 'gray40'))
+plt <- plt +
+    facet_grid(.~chr, space = 'free', scales = 'free')
+
+plt <- plt + scale_size_continuous(limits = c(0, 1), range = c(0, 2))
+
+.aes.lab <- aes(x = (tss + tes)/2, y = theta, label = hgnc)
+
+plt <- plt + geom_text_repel(data = gene.tab.strict, .aes.lab, direction = 'y',
+                      size = 4, segment.color = 'green', color = 'red')
+
+ggsave(filename = 'figures/mediation_gene_effect.pdf', plot = plt, width = 16, height = 8,
+       useDingbats = FALSE, limitsize = FALSE)
+
+################################################################
+## best GWAS vs log-odds
+.df <- gene.tab %>%
+    mutate(gwas.p = pmin(l10.p.two(abs(max.gwas.z)),20)) %>%
+    mutate(twas.p = pmin(l10.p.two(abs(nwas.z)),20))
+
+.df.strict <- .df %>% filter(ensg %in% gene.tab.strict$ensg)
+
+.lodds.cutoff <- min(.df.strict$lodds)
+
+.aes.gwas.lodds <- aes(x = gwas.p, y = lodds)
+
+.grad <- scale_fill_gradientn(colors = c('gray', 'black'), trans = 'log10')
+
+p1 <- ggplot(.df, .aes.gwas.lodds) + theme_bw() + .thm +
+    geom_hex(bins = 50) + xlab('-log10 GWAS') + ylab('logit PIP') +
+    geom_hline(yintercept = .lodds.cutoff, color = 'green', lty = 2) +
+    .grad 
+
+p1 <- p1 + geom_point(data = .df.strict, aes(x = gwas.p, y = lodds), pch = 3, color = 'red') +
+    geom_text_repel(data = .df.strict, aes(x = gwas.p, y = lodds, label = hgnc),
+                    size = 4, direction = 'x', segment.color = 'green', segment.alpha = 0.5)
+
+.aes.gwas.twas <- aes(x = gwas.p, y = twas.p)
+
+.df.twas <- .df %>% filter(lodds < -2, twas.p > -log10(2.5e-6))
+
+p2 <-
+    ggplot(.df, aes(x = twas.p, y = lodds)) + theme_bw() + .thm +
+    geom_hex(bins = 50) + .grad +
+    geom_hline(yintercept = .lodds.cutoff, color = 'green', lty = 2) +
+    geom_vline(xintercept = -log10(2.5e-6), color = 'blue', lty = 2)
+
+p2 <- p2 +
+    geom_point(data = .df.strict, aes(x = twas.p, y = lodds), pch = 3, color = 'red') +
+    geom_point(data = .df.twas, aes(x = twas.p, y = lodds), pch = 1, color = 'blue')
+
+p2 <- p2 +
+    geom_text_repel(data = .df.strict, aes(x = twas.p, y = lodds, label = hgnc),
+                    size = 4, direction = 'x', segment.color = 'green', segment.alpha = 0.5) +
+    geom_text_repel(data = .df.twas, aes(x = twas.p, y = lodds, label = hgnc),
+                    size = 2, nudge_x = 1, nudge_y = -1, segment.color = 'gray80',
+                    color = 'blue', segment.alpha = 0.5)
+
+p2 <- p2 + xlab('-log10 TWAS') + ylab('logit PIP')
+
+pdf(file = 'figures/mediation_gene_twas.pdf', width = 16, height = 8, useDingbats = FALSE)
+print(grid.hcat(list(p1, p2)))
+dev.off()
+
+
 
 ## Figure 1. global effect sizes
 draw.effect.chr <- function(.chr) {
@@ -52,12 +170,7 @@ draw.effect.chr <- function(.chr) {
     genome.length <- genome.range[2] - genome.range[1]
     genome.w <- genome.length / blk.width * 0.05 + 1
 
-    .gwas.mb <- function() {
-        function(x) format(x/1e6, big.mark=',')
-    }
-
-    genome.x.scale <- scale_x_continuous(limits = genome.range + c(-1000, 1000), expand = c(0, 0),
-                                         labels = .gwas.mb())
+    genome.x.scale <- get.genome.scale(.df)
 
     plt <- plt + genome.x.scale
 
@@ -133,12 +246,7 @@ draw.boot.chr <- function(.chr) {
     genome.length <- genome.range[2] - genome.range[1]
     genome.w <- genome.length / blk.width * 0.05 + 1
 
-    .gwas.mb <- function() {
-        function(x) format(x/1e6, big.mark=',')
-    }
-
-    genome.x.scale <- scale_x_continuous(limits = genome.range + c(-1000, 1000), expand = c(0, 0),
-                                         labels = .gwas.mb())
+    genome.x.scale <- get.genome.scale(.df)
 
     plt <- plt + genome.x.scale
 
